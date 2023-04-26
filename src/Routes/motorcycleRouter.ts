c75cd9916ba3e48a77b8c1dce25d995f41c32da8
Rodrigo <rodrigomartins.agro@gmail.com>
2023-04-26 19:43:16 -0300

    feat: add endpoints GET/motorcycles GET/PUT/motorcycles/:id

--- src/Routes/motorcycleRouter.ts
+++ src/Routes/motorcycleRouter.ts
@@ -4,6 +4,11 @@ import MotorcycleController from '../Controllers/MotorcycleController';
 const router = Router();
 
 router.route('/motorcycles')
-  .post((req, res, next) => new MotorcycleController(req, res, next).create());
+  .post((req, res, next) => new MotorcycleController(req, res, next).create())
+  .get((req, res, next) => new MotorcycleController(req, res, next).getAll());
+
+router.route('/motorcycles/:id')
+  .get((req, res, next) => new MotorcycleController(req, res, next).getById())
+  .put((req, res, next) => new MotorcycleController(req, res, next).updateById());
 
 export default router;
